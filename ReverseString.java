c3105cb3e5991a5fdb4f9c1d6d8a6d508e728361
NIKE\EASEER <emilnithya.aseervatham@nike.com>
2018-01-29 13:10:21 -0800

    Reverse a string

---
+++ ReverseString.java
@@ -0,0 +1,19 @@
+public class ReverseString {
+   public static void main(String[] args) {
+      String s = args[0];
+
+      char[] c = s.toCharArray();
+      int n = c.length - 1;
+      for(int i = 0; i <= n/2; i++) {
+//	System.out.print(c[i]+" "+c[n-i]);
+	if(c[i] == c[n-i]) 
+          continue;
+         c[i] = (char) (c[i] ^ c[n-i]);
+	 c[n - i] = (char) (c[i] ^ c[n - i]);
+         c[i] = (char) (c[i] ^ c[n - i]);
+//	System.out.println(c[i]+" "+c[n-i]);
+      }
+	System.out.println(new String(c));
+   }
+
+}
